51fb103781e457190b4b2c6663644fc6d70ca018
Greg Spitz <gregspitz6@gmail.com>
2018-03-09 19:35:24 -0500

    Add license to mock build

--- app/src/mock/java/com/gregspitz/flashcardapp/data/source/local/FakeFlashcardLocalDataSource.java
+++ app/src/mock/java/com/gregspitz/flashcardapp/data/source/local/FakeFlashcardLocalDataSource.java
@@ -1,6 +1,23 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.gregspitz.flashcardapp.data.source.local;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
 
 import com.gregspitz.flashcardapp.data.source.FlashcardDataSource;
 import com.gregspitz.flashcardapp.data.model.Flashcard;
@@ -13,11 +30,16 @@ public class FakeFlashcardLocalDataSource implements FlashcardDataSource {
 
     private static FakeFlashcardLocalDataSource INSTANCE;
 
-    private FakeFlashcardLocalDataSource() {}
+    /**
+     * Constructor, private to avoid instantiation
+     * @param context included for consistency with the real version
+     */
+    @VisibleForTesting
+    public FakeFlashcardLocalDataSource(@NonNull Context context) {}
 
-    public static FakeFlashcardLocalDataSource getInstance() {
+    public static FakeFlashcardLocalDataSource getInstance(@NonNull Context context) {
         if (INSTANCE == null) {
-            INSTANCE = new FakeFlashcardLocalDataSource();
+            INSTANCE = new FakeFlashcardLocalDataSource(context);
         }
         return INSTANCE;
     }
--- app/src/mock/java/com/gregspitz/flashcardapp/data/source/remote/FakeFlashcardRemoteDataSource.java
+++ app/src/mock/java/com/gregspitz/flashcardapp/data/source/remote/FakeFlashcardRemoteDataSource.java
@@ -1,6 +1,22 @@
+/*
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.gregspitz.flashcardapp.data.source.remote;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
 
 import com.google.common.collect.Lists;
 import com.gregspitz.flashcardapp.data.source.FlashcardDataSource;
@@ -22,7 +38,8 @@ public class FakeFlashcardRemoteDataSource implements FlashcardDataSource {
     public static final String FAILED_SAVE_FRONT = "failed_save";
 
     // prevent direct instantiation
-    private FakeFlashcardRemoteDataSource() {}
+    @VisibleForTesting
+    public FakeFlashcardRemoteDataSource() {}
 
     public static FakeFlashcardRemoteDataSource getInstance() {
         if (INSTANCE == null) {
